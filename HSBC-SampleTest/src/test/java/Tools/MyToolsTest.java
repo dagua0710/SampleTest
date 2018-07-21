c5e43c6734e7c874b5ac354cc0030b2af70362a5
dagua <dagua@DESKTOP-RR6CACG>
2018-07-21 13:00:22 +0800

    modify Unit Test

---
+++ HSBC-SampleTest/src/test/java/Tools/MyToolsTest.java
@@ -0,0 +1,50 @@
+package Tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyToolsTest {
+
+	@BeforeClass
+	public static void beforeClass(){
+	 	System.out.println("before class..");
+	 }
+
+	
+	@Before
+    public void Before() throws Exception {
+		System.out.println("before");  
+    }
+	
+	@Test
+	public void testCheckNum3() {
+		assertEquals(true, MyTools.checkNum3(3));
+		//assertEquals(false, MyTools.checkNum3(3));
+	}
+	
+	@Test
+	public void testCheckNum5() {
+		//assertEquals(true, MyTools.checkNum5(5));
+		assertEquals(false, MyTools.checkNum5(5));
+	}
+	
+	@Test(timeout = 10)
+    public void testTimeout() {
+		assertEquals(true, MyTools.checkNum3(3));
+    }
+
+	@After    
+    public void after() {     
+        System.out.println("after");     
+    } 
+	
+	@AfterClass
+	public static void afterClass(){
+		System.out.println("after class..");
+	}
+}
